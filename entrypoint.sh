422a637f4880403eaf469d6e2edbd864e90f562e
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2020-08-11 16:55:41 +0800

    cleanedout entry point

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,19 @@
 
 set -e
 
-if [ "$#" -gt 0 ]; then
-    TMC_API_TOKEN=${TMC_API_TOKEN?"TMC_API_TOKEN not set"}
-    tmc login --name default -c
-    tmc system context use default
-    echo Launching webhook server
-    python3 server.py
+if [ "$#" -eq 0 ]; then
+
+  [[ -z $TMC_API_TOKEN ]] && echo "Environment variable TMC_API_TOKEN  not provided." 1>&2 && exit 1
+  echo "API Token present in environment"
+  
+  tmc login --name default -c
+  echo "TMC login successful"
+  
+  tmc system context use default
+  echo "TMC context switched"
+
+  echo "Launching webhook server"
+  python3 server.py
 else
   exec "$@"
 fi
